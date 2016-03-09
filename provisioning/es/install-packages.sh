dfc667781ba09c62267af40328b9625fafdd0536
emag <lanabe.lanabe@gmail.com>
2016-03-09 13:26:38 +0900

    Add kibana

--- provisioning/es/install-packages.sh
+++ provisioning/es/install-packages.sh
@@ -2,3 +2,12 @@
 
 yum -y install java-1.8.0-openjdk-devel
 yum -y install https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.2.0/elasticsearch-2.2.0.rpm
+
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+cp /vagrant/provisioning/es/kibana.repo /etc/yum.repos.d
+yum -y install kibana
+
+cd /tmp
+wget https://download.elasticsearch.org/elastic/sense/sense-latest.tar.gz
+/opt/kibana/bin/kibana plugin -i sense -u file:///tmp/sense-latest.tar.gz
+chown -R kibana:kibana /opt/kibana
