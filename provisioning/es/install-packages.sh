42f78c70ad70cc3563ea041c017458551bfa7e24
emag <lanabe.lanabe@gmail.com>
2016-07-29 09:17:11 +0900

    Update ES to 2.3.4

--- provisioning/es/install-packages.sh
+++ provisioning/es/install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=2.2.0
+VERSION=2.3.4
 
 yum -y install java-1.8.0-openjdk-devel
 yum -y install https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/${VERSION}/elasticsearch-${VERSION}.rpm
